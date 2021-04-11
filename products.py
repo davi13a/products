35e37132a03522a59c1b4de100ca5d305352fd02
David Lin <davi13a@gmail.com>
2021-04-11 23:27:07 +0800

    add continue

--- products.py
+++ products.py
@@ -1,4 +1,21 @@
 products = []
+#讀取檔案
+#strip去換行符號，split以逗號切割
+#continue符合條件跳過到下一迴
+with open('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue
+        #s = line.strip().split(',')
+        #name = s[0]
+        #price = s[1]
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
+        #print(s)
+
+
+
 while True:
     name = input('請輸入商品名稱（q為結束）： ')
     if name == 'q':
@@ -19,12 +36,12 @@ for pp in products:
     print(pp[0], '的價格是', pp[1])
 
 
-with open('products.txt', 'w') as f:
+#with open('products.txt', 'w') as f:
+ #   for p in products:
+  #      f.write(p[0] + ' 的價格是： ' + p[1] + '\n')
+
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
     for p in products:
-        f.write(p[0] + ' 的價格是： ' + p[1] + '\n')
-
-
-#with open('products.csv', 'w',encoding='utf-8') as f:
-    #f.write('商品,價格\n')
-    #for p in products:
-        #f.write(p[0] + ',' + p[1] + '\n')
+        f.write(p[0] + ',' + p[1] + '\n')
